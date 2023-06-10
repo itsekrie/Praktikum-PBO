b1db73d3e3fa499c1f62fe35a0f5d3f59d0813e9
itsekrie <92426973+itsekrie@users.noreply.github.com>
2023-06-10 14:02:10 +0700

    Add files via upload

---
+++ Pertemuan 3/org/poligon/Poligon.java
@@ -0,0 +1,18 @@
+/**
+* File : MPoligon.java 15/03/23
+* Penulis : Fahriant Ekrie/24060121130071
+* Deskripsi : representasi dasar dari objek poligon (segi banyak)
+*/
+
+
+package org.poligon;
+
+public class Poligon{
+		protected int jumlahSisi;
+		
+		public int getJumlahSisi(){
+			return this.jumlahSisi;
+		}
+		
+	
+	}
