947f576e7f2cdcb2b284a53d9aad0e85e5d48f4f
itsekrie <92426973+itsekrie@users.noreply.github.com>
2023-06-10 14:14:52 +0700

    Update PersonDAO.java

--- Pertemuan 9/PersonDAO.java
+++ Pertemuan 9/PersonDAO.java
@@ -1,6 +1,8 @@
 /**
 * Nama File : PersonDAO.java 
 * Dekripsi 	: interface untuk person access object
+ * Nama Pembuat 	: Fahriant Ekrie
+ * NIM			: 24060121130071
 **/
 
 public interface PersonDAO{
